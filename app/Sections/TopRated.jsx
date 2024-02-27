ac4b643be2e4ab1fa4c903885901a02037e8a5d2
Milad Joodi <winbeta.com@gmail.com>
2024-02-28 00:48:02 +0330

    0

--- app/Sections/TopRated.jsx
+++ app/Sections/TopRated.jsx
@@ -19,36 +19,39 @@ const TopRated = () => {
     })
   }, [])
 
-  var settings = {
-    dots: true,
-    infinite: false,
+  var settings = { 
+    dots: false,
+    infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false,
+          arrows:false
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToScroll: 1,
+          initialSlide: 1,
+          arrows: false
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: false
         }
       }
     ]
