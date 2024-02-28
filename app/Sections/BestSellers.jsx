932fc3e62a192a010a6bdc8c37c323772ca99987
Milad Joodi <winbeta.com@gmail.com>
2024-02-28 16:45:43 +0330

    0

--- app/Sections/BestSellers.jsx
+++ app/Sections/BestSellers.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+// import { useDarkMode } from '../DarkModeContext'
+import { pizza } from '../export'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Image from 'next/image'
+import { useDarkMode } from '../DarkModeContext'
 
 function BestSellers() {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100
+    })
+  }, [])
+
+  const { darkMode, toggleDarkMode } = useDarkMode()
+
+
   return (
-    <div>BestSellers</div>
+    <section className={`${darkMode? 'dark bg-black' : 'light bg-black'} w-full lg:px-20px px-10 lg:py-20 py-10 h-fit flex flex-col justify-center items-center gap-14`}>
+      <h1 data-aos="slide-up" className='text-white text-6xl font-bold text-center'>Our Best Sellers</h1>
+      <div data-aos="slide-up" className='grid lg:grid-cols-3 grid-cols-1 justify-between items-center gap-6'>
+        {pizza.map((item, index) => (
+          <div key={index} className='bg-white dark:bg-gay-800 p-8 flex flex-col justify-center items-center gap-4 rounded-lg'>
+            <Image
+              src={item.image}
+              alt="product image"
+              className='rounded-lg transform hover:scale-105 transition-transform duration-300 cursor-pointer'
+            />
+            <h1 className='text-black text-center text-2xl font-bold dark:text-white'>{item.title}</h1>
+            <h1 className='text-orange-600 font-bold text-3xl'>{item.price}</h1>
+            <p className='text-lg text-slate-800 dark:text-white text-center'>{item.para}</p>
+            <buttom className='bg-orange-500 text-white px-8 py-3 rounded-full font-semibold hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black cursor-pointer'>
+              ORDER NOW
+            </buttom>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- app/Sections/Faq.jsx
+++ app/Sections/Faq.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useDarkMode } from '../DarkModeContext'
 
 function Faq() {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100
+    })
+  }, [])
+
+  const { darkMode, toggleDarkMode } = useDarkMode()
+
   return (
-    <div>Faq</div>
+    <section id='faq' className={`${darkMode ? 'dark bg-black' : 'light bg-light'} w-full lg:px-20 px-10 lg:py-20 py-10 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-14`}>
+      
+      {/*LEFT  */}
+      <div id='left' className='flex flex-col justify-center items-center gap-12'>
+      <h1 id='left' className='text-6xl text-black font-bold text-center dark:text-white'>Frequently Asked Questions</h1>
+      <div data-aos='slide-up' className='w-full'>
+        
+      </div>
+      </div>
+
+      {/*RIGHT  */}
+      <div>
+        <div id='image box'></div>
+      </div>
+
+    </section>
   )
 }
 
